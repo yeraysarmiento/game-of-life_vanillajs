1b93bb4b42e954370d22e6325d2d82f01a7614cc
ysarmiento <yeray.sarmiento@hotmail.com>
2021-10-09 16:28:05 +0200

    Add createBoard() test

--- gameOfLife.test.js
+++ gameOfLife.test.js
@@ -1,4 +1,4 @@
-const createBoard = require("./gameOfLife");
+const { createBoard } = require("./gameOfLife");
 
 describe("Given the function createBoard()", () => {
   describe("When it receives a number 5 and a number 5", () => {
@@ -18,7 +18,30 @@ describe("Given the function createBoard()", () => {
       const result = createBoard(row, col);
 
       // Assert
-      expect(result).toBe(expected);
+      expect(result).toEqual(expected);
+    });
+  });
+});
+
+describe("Given the function createBoard()", () => {
+  describe("When it receives a number 5 and a number 5", () => {
+    test("Then it should return [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]'", () => {
+      // Arrange
+      const row = 5;
+      const col = 5;
+      const expected = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ];
+
+      // Act
+      const result = createBoard(row, col);
+
+      // Assert
+      expect(result).toEqual(expected);
     });
   });
 });
