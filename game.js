ab6470be5cba99162883674ada9998f6dd06c92b
ysarmiento <yeray.sarmiento@hotmail.com>
2021-10-09 10:34:38 +0200

    Add whatNeigh & countNeigh functions

--- game.js
+++ game.js
@@ -24,19 +24,21 @@ const board = [
 
 // Recorrer el tablero:
 function iterateBoard(array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      if (i === 0 && )
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < array.length; j += 1) {
+      countNeighbours(i, j);
     }
   }
+  return array;
 }
 
-iterateBoard(array);
+iterateBoard(board);
 
 // Clasificar según cuántos vecinos tenga: 3, 5, 8:
 // Contar los vecinos vivos/muertos de cada celda cuando sean esquinas:
 // Contar los vecinos vivos/muertos de cada celda cuando sean extremo no esquina:
 // Contar los vecinos vivos/muertos de cada celda central:
+
 // Aplicar las normas del juego:
 // Si esta viva:
 // Si 2 o 3 vecinos vivos - VIVE
@@ -47,54 +49,24 @@ iterateBoard(array);
 
 // Generar un nuevo array con las celdas cambiados.
 
-function whatNeighbour(array) {
-  try {
-    return array[row][col]; // funcion que analice la celda
-  } catch {
+// Función que descarta celdas/columnas fuera del tablero y devuelve el valor de las celdas:
+function whatNeighbour(array, row, col) {
+  if (row < 0 || row > array.length || col < 0 || col > array.length) {
     return 0;
   }
+  return array[row][col];
 }
 
-countNeighbours = (row, col) => {
-  let total_neighbours = 0;
-  total_neighbours += whatNeighbour(row - 1, col - 1);
-  total_neighbours += whatNeighbour(row - 1, col);
-  total_neighbours += whatNeighbour(row - 1, col + 1);
-  total_neighbours += whatNeighbour(row, col - 1);
-  total_neighbours += whatNeighbour(row, col + 1);
-  total_neighbours += whatNeighbour(row + 1, col - 1);
-  total_neighbours += whatNeighbour(row + 1, col);
-  total_neighbours += whatNeighbour(row + 1, col + 1);
-  return total_neighbours;
+// Dada una coordenada row y col busca a sus vecinos y le aplica una función:
+countNeighbours = (array, row, col) => {
+  let sumNeighbours = 0;
+  sumNeighbours += whatNeighbour(row - 1, col - 1);
+  sumNeighbours += whatNeighbour(row - 1, col);
+  sumNeighbours += whatNeighbour(row - 1, col + 1);
+  sumNeighbours += whatNeighbour(row, col - 1);
+  sumNeighbours += whatNeighbour(row, col + 1);
+  sumNeighbours += whatNeighbour(row + 1, col - 1);
+  sumNeighbours += whatNeighbour(row + 1, col);
+  sumNeighbours += whatNeighbour(row + 1, col + 1);
+  return sumNeighbours;
 };
-
-const array = [
-  [0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 0],
-  [0, 0, 1, 0, 0],
-  [0, 0, 1, 0, 0],
-  [0, 0, 0, 0, 0],
-];
-
-const arr = [];
-function hola(array) {
-  for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
-      // row 1
-      arr[i - 1][j - 1] = array[i - 1][j - 1];
-      arr[i - 1][j] = array[i - 1][j];
-      arr[i - 1][j + 1] = array[i - 1][j + 1];
-      // row 2
-      arr[i][j - 1] = array[i][j - 1];
-      arr[i][j] = array[i][j];
-      arr[i][j + 1] = array[i][j + 1];
-      // row 3
-      arr[i + 1][j - 1] = array[i + 1][j - 1];
-      arr[i + 1][j] = array[i + 1][j];
-      arr[i + 1][j + 1] = array[i + 1][j + 1];
-    }
-  }
-  return arr;
-}
-
-hola(array);
