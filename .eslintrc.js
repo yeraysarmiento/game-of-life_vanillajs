040dff120cb4eccd35855cc8816fca8cf3d13cbd
ysarmiento <yeray.sarmiento@hotmail.com>
2021-10-09 09:48:06 +0200

    features/function

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
-    jest: true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: [
+    'airbnb-base',
+  ],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    "no-param-reassign": "off",
-    "no-plusplus": "off",
-    "no-restricted-syntax": "off",
-    "consistent-return": "off",
-    "no-debugger": "off",
   },
 };
--- game.js
+++ game.js
@@ -1,8 +1,20 @@
-// Any live cell with two or three live neighbours survives.
-// Any dead cell with three live neighbours becomes a live cell.
-// All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+// Crear el board de juego con todo resetado a 0.
 
-const array = [
+let row;
+let col;
+
+function createBoard(row, col) {
+  const board = [];
+  for (let i = 0; i < row; i++) {
+    board[i] = [];
+    for (let j = 0; j < col; j++) {
+      board[i][j] = 0;
+    }
+  }
+  return board;
+}
+
+const board = [
   [0, 0, 0, 0, 0],
   [0, 0, 1, 0, 0],
   [0, 0, 1, 0, 0],
@@ -10,16 +22,17 @@ const array = [
   [0, 0, 0, 0, 0],
 ];
 
-// Recorrer el tablero y clasificar segun el numero de vecinos:
+// Recorrer el tablero:
 function iterateBoard(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
-      array[i][j] = 2;
+      if (i === 0 && )
     }
   }
 }
 
 iterateBoard(array);
+
 // Clasificar según cuántos vecinos tenga: 3, 5, 8:
 // Contar los vecinos vivos/muertos de cada celda cuando sean esquinas:
 // Contar los vecinos vivos/muertos de cada celda cuando sean extremo no esquina:
@@ -28,7 +41,6 @@ iterateBoard(array);
 // Si esta viva:
 // Si 2 o 3 vecinos vivos - VIVE
 // Else - MUERE
-
 // Si está muerta:
 // Si 3 vecinos vivos - VIVE
 // Else - MUERE
@@ -64,23 +76,19 @@ const array = [
   [0, 0, 0, 0, 0],
 ];
 
-function iterateBoard(array) {
-  const arr = [
-    [0, 0],
-    [0, 0],
-    [0, 0],
-  ];
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      //row 1
+const arr = [];
+function hola(array) {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      // row 1
       arr[i - 1][j - 1] = array[i - 1][j - 1];
       arr[i - 1][j] = array[i - 1][j];
       arr[i - 1][j + 1] = array[i - 1][j + 1];
-      //row 2
+      // row 2
       arr[i][j - 1] = array[i][j - 1];
       arr[i][j] = array[i][j];
       arr[i][j + 1] = array[i][j + 1];
-      //row 3
+      // row 3
       arr[i + 1][j - 1] = array[i + 1][j - 1];
       arr[i + 1][j] = array[i + 1][j];
       arr[i + 1][j + 1] = array[i + 1][j + 1];
@@ -89,13 +97,4 @@ function iterateBoard(array) {
   return arr;
 }
 
-iterateBoard(array);
-
-for (let i = 0; i < 5; i++) {
-  active_array[i] = [];
-  for (let j = 0; j < 5; j++) {
-    active_array[i][j] = 0;
-  }
-}
-
-console.log(active_array);
+hola(array);
