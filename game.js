702a59e203499cc0c4f4954b97cea81f730621b6
ysarmiento <yeray.sarmiento@hotmail.com>
2021-10-09 12:09:12 +0200

    Add countCells function

--- game.js
+++ game.js
@@ -1,18 +1,4 @@
-// Crear el board de juego con todo resetado a 0.
-
-let row;
-let col;
-
-function createBoard(row, col) {
-  const board = [];
-  for (let i = 0; i < row; i++) {
-    board[i] = [];
-    for (let j = 0; j < col; j++) {
-      board[i][j] = 0;
-    }
-  }
-  return board;
-}
+//La suma de los valores sus vecinos nos da la cantidad de celdas vivas/muertas, comparo el valor suma con el central para aplicar norma
 
 const board = [
   [0, 0, 0, 0, 0],
@@ -21,23 +7,55 @@ const board = [
   [0, 0, 1, 0, 0],
   [0, 0, 0, 0, 0],
 ];
+let row;
+let col;
 
-// Recorrer el tablero:
-function iterateBoard(array) {
-  for (let i = 0; i < array.length; i += 1) {
-    for (let j = 0; j < array.length; j += 1) {
-      countNeighbours(i, j);
+// Función que crea el board de juego con todo resetado a 0.
+function createBoard(row, col) {
+  const board = [];
+  for (let i = 0; i < row; i += 1) {
+    board[i] = [];
+    for (let j = 0; j < col; j += 1) {
+      board[i][j] = 0;
     }
   }
-  return array;
+  return board;
 }
 
-iterateBoard(board);
+// Función que recorre el tablero:
+function iterateBoard(board) {
+  for (row = 0; row < board.length; row += 1) {
+    for (col = 0; col < board.length; col += 1) {
+      countCells(board, row, col);
+    }
+  }
+  return board;
+}
 
-// Clasificar según cuántos vecinos tenga: 3, 5, 8:
-// Contar los vecinos vivos/muertos de cada celda cuando sean esquinas:
-// Contar los vecinos vivos/muertos de cada celda cuando sean extremo no esquina:
-// Contar los vecinos vivos/muertos de cada celda central:
+// Función que descarta celdas/columnas fuera del tablero y devuelve el valor de las celdas:
+function whatCell(array, row, col) {
+  if (row < 0 || row > array.length || col < 0 || col > array.length) {
+    return 0;
+  }
+  return array[row][col];
+}
+
+// Dada una coordenada row y col busca a sus vecinos y le aplica una función:
+countCells = (array, row, col) => {
+  const sumCells = 0;
+  // Fila superior
+  sumNeighbours += whatCell(row - 1, col - 1);
+  sumNeighbours += whatCell(row - 1, col);
+  sumNeighbours += whatCell(row - 1, col + 1);
+  // Fila central
+  sumNeighbours += whatCell(row, col - 1);
+  sumNeighbours += whatCell(row, col + 1);
+  // Fila inferior
+  sumNeighbours += whatCell(row + 1, col - 1);
+  sumNeighbours += whatCell(row + 1, col);
+  sumNeighbours += whatCell(row + 1, col + 1);
+  return sumCells;
+};
 
 // Aplicar las normas del juego:
 // Si esta viva:
@@ -48,25 +66,3 @@ iterateBoard(board);
 // Else - MUERE
 
 // Generar un nuevo array con las celdas cambiados.
-
-// Función que descarta celdas/columnas fuera del tablero y devuelve el valor de las celdas:
-function whatNeighbour(array, row, col) {
-  if (row < 0 || row > array.length || col < 0 || col > array.length) {
-    return 0;
-  }
-  return array[row][col];
-}
-
-// Dada una coordenada row y col busca a sus vecinos y le aplica una función:
-countNeighbours = (array, row, col) => {
-  let sumNeighbours = 0;
-  sumNeighbours += whatNeighbour(row - 1, col - 1);
-  sumNeighbours += whatNeighbour(row - 1, col);
-  sumNeighbours += whatNeighbour(row - 1, col + 1);
-  sumNeighbours += whatNeighbour(row, col - 1);
-  sumNeighbours += whatNeighbour(row, col + 1);
-  sumNeighbours += whatNeighbour(row + 1, col - 1);
-  sumNeighbours += whatNeighbour(row + 1, col);
-  sumNeighbours += whatNeighbour(row + 1, col + 1);
-  return sumNeighbours;
-};
